8c4bfba9edd3bcc88bdbeac32782b19acf884ff0
K S MITHUN <mithunmpire@gmail.com>
2026-01-25 18:19:32 +0530

    website update 17
    blogs backend - sanity

---
+++ sanity/env.js
@@ -0,0 +1,7 @@
+export const apiVersion =
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2026-01-25'
+
+// export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+export const dataset = "production";
+// export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+export const projectId = "xmv2v3r2";
