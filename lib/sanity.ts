2b443406492c0ba2faedb9a5d76cedae19508e1f
K S MITHUN <mithunmpire@gmail.com>
2026-01-26 20:01:38 +0530

    update sanity.ts for refreshing content

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -5,7 +5,7 @@ export const sanityClient = createClient({
   projectId: 'xmv2v3r2', // 👈 same as sanity.config.ts
   dataset: 'production',
   apiVersion: '2024-01-01',
-  useCdn: true,
+  useCdn: false,
 })
 
 const builder = imageUrlBuilder(sanityClient)
