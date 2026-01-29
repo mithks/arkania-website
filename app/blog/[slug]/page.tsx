4f4dc6268463d84124348571946f4133f0b9159a
K S MITHUN <mithunmpire@gmail.com>
2026-01-29 10:08:18 +0530

    updated page width- blog details

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -36,50 +36,52 @@ export default async function BlogPost({
   }
 
   return (
-    <article className="max-w-[1000px] mx-auto px-6 md:px-8 py-48 bg-light">
+    <section className="w-full bg-light">
+      <article className="max-w-[1500px] mx-auto px-6 md:px-8 py-48 bg-light">
 
-      {/* Meta */}
-      <p className="text-[20px] text-dark/60 mb-6">
-        {post.publishedAt && new Date(post.publishedAt).toDateString()}
-        {post.author?.name && ` • by ${post.author.name}`}
-      </p>
+        {/* Meta */}
+        <p className="text-[20px] text-dark/60 mb-6">
+          {post.publishedAt && new Date(post.publishedAt).toDateString()}
+          {post.author?.name && ` • by ${post.author.name}`}
+        </p>
 
-      {/* Title */}
-      <h1 className="text-[64px] font-bold text-dark leading-tight mb-12">
-        {post.title}
-      </h1>
+        {/* Title */}
+        <h1 className="text-[64px] font-bold text-dark leading-tight mb-12">
+          {post.title}
+        </h1>
 
-      {/* Main Image */}
-      {post.mainImage && (
-        <img
-          src={urlFor(post.mainImage).width(1200).url()}
-          alt={post.title}
-          className="rounded-3xl mb-16"
-        />
-      )}
-
-      {/* Content */}
-      <div className="prose prose-xl max-w-none text-dark">
-        <PortableTextRenderer value={post.body} />
-      </div>
-
-      <div className="flex items-center gap-4 mt-6">
-        {post.author?.image && (
+        {/* Main Image */}
+        {post.mainImage && (
           <img
-            src={urlFor(post.author.image).width(48).height(48).url()}
-            alt={post.author.name}
-            className="rounded-full"
+            src={urlFor(post.mainImage).width(1200).url()}
+            alt={post.title}
+            className="rounded-3xl mb-16"
           />
         )}
 
-        <div>
-        <p className="font-medium text-gray-500">
-          {post.author?.name || 'Arkania Team'}
-        </p>
-          <p className="text-sm text-gray-500">Author</p>
+        {/* Content */}
+        <div className="prose prose-xl max-w-none text-dark">
+          <PortableTextRenderer value={post.body} />
         </div>
-      </div>
 
-    </article>
+        <div className="flex items-center gap-4 mt-6">
+          {post.author?.image && (
+            <img
+              src={urlFor(post.author.image).width(48).height(48).url()}
+              alt={post.author.name}
+              className="rounded-full"
+            />
+          )}
+
+          <div>
+          <p className="font-medium text-gray-500">
+            {post.author?.name || 'Arkania Team'}
+          </p>
+            <p className="text-sm text-gray-500">Author</p>
+          </div>
+        </div>
+
+      </article>
+    </section>
   )
 }
