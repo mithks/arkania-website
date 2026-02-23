105220dfcc196cc955ef23f73156a2acab1a91b1
K S MITHUN <mithunmpire@gmail.com>
2026-02-23 17:01:33 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,39 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: [],
+  async redirects() {
+  return [
+  {
+  source: '/about',
+  destination: '/',
+  permanent: true,
   },
-}
-
-module.exports = nextConfig
+  {
+  source: '/blog',
+  destination: '/',
+  permanent: true,
+  },
+  {
+  source: '/services/ewm',
+  destination: '/',
+  permanent: true,
+  },
+  {
+  source: '/services/tm',
+  destination: '/',
+  permanent: true,
+  },
+  {
+  source: '/services/custom',
+  destination: '/',
+  permanent: true,
+  },
+  {
+  source: '/services/ai',
+  destination: '/',
+  permanent: true,
+  }
+  ]
+  },
+  }
+  
+  module.exports = nextConfig
