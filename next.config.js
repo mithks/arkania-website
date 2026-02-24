eece37223558a2908e3d6d281521361843d390c5
K S MITHUN <mithunmpire@gmail.com>
2026-02-24 14:18:56 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,39 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-  return [
-  {
-  source: '/about',
-  destination: '/about',
-  permanent: true,
-  },
-  {
-  source: '/blog',
-  destination: '/blog',
-  permanent: true,
-  },
-  {
-  source: '/services/ewm',
-  destination: '/services/ewm',
-  permanent: true,
-  },
-  {
-  source: '/services/tm',
-  destination: '/services/tm',
-  permanent: true,
-  },
-  {
-  source: '/services/custom',
-  destination: '/services/custom',
-  permanent: true,
-  },
-  {
-  source: '/services/ai',
-  destination: '/services/ai',
-  permanent: true,
-  }
-  ]
-  },
+  
   }
   
   module.exports = nextConfig
